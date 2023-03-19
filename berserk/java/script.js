8dc51a74312489ac349b6e14e2942901895acfa7
Marco Antonio <126245479+tch0la@users.noreply.github.com>
2023-03-18 22:22:44 -0300

    Add files via upload

---
+++ berserk/java/script.js
@@ -0,0 +1,171 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.indextext', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.apguts', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psguts', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsguts', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.apGriffith', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psGriffith', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsGriffith', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.apCasca', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psCasca', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsCasca', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.hsRickert', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.apRickert', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psRickert', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.apJudeau', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psJudeau', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsJudeau', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.apPippin', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psPippin', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsPippin', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.apCorkus', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psCorkus', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsCorkus', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+sr.reveal('.apGaston', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.psGaston', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+sr.reveal('.hsGaston', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 2000
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sr.reveal('.Personalidade', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 1000
+});
+
+sr.reveal('.titu', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 1000
+});
+
+sr.reveal('.historia', {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 1000
+});
